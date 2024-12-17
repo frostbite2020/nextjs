caa24d8c3d29eebcdac000d7e54c33f48447ab85
Rafi Ardiansyah <ardiansyahrafi4@gmail.com>
2024-12-17 12:53:48 +0700

    ✨ : Integration with Sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,54 @@
-import Hello from "@/app/components/hello";
-import Navbar from "../components/Navbar";
+import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { query?: string };
+}) {
+  const query = (await searchParams).query;
+
+  // Jika menggunakan useSearchParams harus menggunakan 'use client'
+  // const searchParams = useSearchParams();
+  // const query = searchParams.get("query");
+
+  const params = { search: query || null };
+
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
   return (
     <>
-      <h1 className="heading">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semi-bold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p>No startups found</p>
+          )}
+        </ul>
+      </section>
+
+      <SanityLive />
     </>
   );
 }
---
+++ app/(root)/startup/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      Startup Detail
+    </div>
+  )
+}
+
+export default Page
